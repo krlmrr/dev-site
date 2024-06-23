774329c27dc89ec1e4677ff3712bd37a81a70d04
Karl Murray <karl@karlm.dev>
2024-06-23 14:41:27 -0500

    Install duster and run fix.

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    protected $guarded= [
+    protected $guarded = [
         'id',
         'created_at',
         'updated_at',
