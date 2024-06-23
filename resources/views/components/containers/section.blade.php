45f10e0519756efec69974c10d6dfff91c31fd37
Karl Murray <karl@karlm.dev>
2024-06-23 18:30:34 -0500

    Getting more features done!

--- resources/views/components/containers/section.blade.php
+++ resources/views/components/containers/section.blade.php
@@ -4,15 +4,15 @@
 ])
 
 <section
-    class="flex justify-center"
+    class="flex justify-center scroll-mt-20 lg:scroll-mt-4"
     id="{{ $title }}"
 >
-    <div class="flex flex-col gap-4 mx-4 lg:mx-8 bg-gray-50 dark:bg-gray-800 max-w-7xl px-4 py-4 rounded-md drop-shadow w-full">
+    <div class="flex flex-col gap-2 mx-4 lg:mx-8 bg-gray-50 dark:bg-gray-800 max-w-7xl px-4 py-4 rounded-md drop-shadow w-full">
         @isset($title)
             <h2 class="text-lg font-semibold">{{ $title }}</h2>
         @endisset
         @isset($body)
-            <p class="prose dark:prose-invert list-disc">
+            <p class="prose dark:prose-invert">
                 {!! $body !!}
             </p>
         @endisset
