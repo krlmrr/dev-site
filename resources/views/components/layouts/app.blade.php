0ad5ceee0a485d658a6f92e5e7487ad43d24f3e1
Karl Murray <karl@karlm.dev>
2024-06-23 14:26:17 -0500

    git init

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>
+            {{ isset($title)
+       ? $title . ' | ' . config('app.name', 'Laravel')
+       : config('app.name', 'Laravel')
+   }}
+        </title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="flex flex-col min-h-screen font-sans antialiased bg-gray-100 dark:bg-gray-900">
+        <main class="flex-grow">
+            {{ $slot }}
+        </main>
+    </body>
+</html>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,10 @@
+<?php
+
+use function Livewire\Volt\{title};
+
+title('Home');
+?>
+
+<div class="mx-4">
+    <p>here</p>
+</div>
