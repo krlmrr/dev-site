6c93af434ab228a3ebee3a47ebe80b97661a445a
Karl Murray <karl@karlm.dev>
2024-06-23 14:49:18 -0500

    Composer update

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -2,21 +2,29 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1"
+        >
 
         <title>
             {{ isset($title)
-       ? $title . ' | ' . config('app.name', 'Laravel')
-       : config('app.name', 'Laravel')
-   }}
+                 ? $title . ' | ' . config('app.name', 'Laravel')
+                 : config('app.name', 'Laravel')
+            }}
         </title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link
+            rel="preconnect"
+            href="https://fonts.bunny.net"
+        >
+        <link
+            href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap"
+            rel="stylesheet"
+        />
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="flex flex-col min-h-screen font-sans antialiased bg-gray-100 dark:bg-gray-900">
+    <body class="flex flex-col min-h-screen font-sans antialiased bg-gray-100 dark:bg-gray-900 dark:text-gray-100">
         <main class="flex-grow">
             {{ $slot }}
         </main>
